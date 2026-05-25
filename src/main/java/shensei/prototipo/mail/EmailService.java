92c33406a70c49f8b2d37cf290771353fd75565c
Nelson Tepeu <ntepeup@miumg.edu.gt>
2026-05-25 04:24:30 -0600

    configuracion de correos

--- src/main/java/shensei/prototipo/mail/EmailService.java
+++ src/main/java/shensei/prototipo/mail/EmailService.java
@@ -1,5 +1,6 @@
 package shensei.prototipo.mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,30 +14,41 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${app.mail.from}")
+    private String from;
+
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
+    // CORREO SIMPLE
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+
         mailSender.send(message);
     }
 
+    // CORREO HTML
     public void sendHtmlMessage(String to, String subject, String html) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+
+            helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(html, true);
+
             mailSender.send(message);
+
         } catch (MessagingException e) {
             throw new RuntimeException("No se pudo enviar correo HTML", e);
         }
     }
 }
-
 
